34d0f762d56772c97ac8fccba10cc906ccc564c9
Bruno Martins <bgcngm@gmail.com>
2025-04-29 23:37:50 +0100

    zenfone7: Migrate Lineage Health to soong_config_set
    Change-Id: I2eff39e83b9673aa2989d05de65c646241ba61da
    Signed-off-by: micky387 <mickaelsaibi@free.fr>

--- device.mk
+++ device.mk
@@ -307,12 +307,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
-$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/charging_enabled)
-$(call soong_config_set,lineage_health,charging_control_charging_enabled,1)
-$(call soong_config_set,lineage_health,charging_control_charging_disabled,0)
-$(call soong_config_set,lineage_health,charging_control_charging_bypass,true)
-$(call soong_config_set,lineage_health,charging_control_charging_toggle,true)
-$(call soong_config_set,lineage_health,charging_control_charging_deadline,false)
+$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/device/smartchg_stop_charging)
+$(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
+$(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
 
 # HIDL
 PRODUCT_PACKAGES += \
