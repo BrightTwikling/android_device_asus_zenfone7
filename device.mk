e74259b355e92346cc8afdfca723217fe77cc609
BrightTwikling <DFSJbbba5@gmail.com>
2025-05-01 13:10:18 +0900

    Build some OMX packages
    Signed-off-by: BrightTwikling <DFSJbbba5@gmail.com>

--- device.mk
+++ device.mk
@@ -198,7 +198,15 @@ PRODUCT_PACKAGES += \
     libvulkan \
     vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.display.composer-service \
-    vendor.qti.hardware.memtrack-service
+    vendor.qti.hardware.memtrack-service \
+    libcrypto_shim \
+    libOmxAacEnc \
+    libOmxAmrEnc \
+    libOmxEvrcEnc \
+    libOmxG711Enc \
+    libOmxQcelp13Enc \
+    libOmxVdec \
+    libOmxVenc
 
 -include hardware/qcom-caf/sm8250/display/config/display-board.mk
 
