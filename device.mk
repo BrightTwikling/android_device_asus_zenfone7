82b3b68d8230e4d320b99177ca275da366ef8255
BrightTwikling <DFSJbbba5@gmail.com>
2025-05-17 21:08:58 +0900

    Media: Add PRODUCT_PACKAGES
    Signed-off-by: BrightTwikling <DFSJbbba5@gmail.com>

--- device.mk
+++ device.mk
@@ -358,6 +358,10 @@ PRODUCT_PACKAGES += \
     librs_jni
 
 # Media
+PRODUCT_PACKAGES += \
+    android.hardware.media.c2@1.0 \
+    android.hardware.media.omx@1.0
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/media_codecs_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml \
     $(LOCAL_PATH)/media/media_codecs_kona.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_kona.xml \
