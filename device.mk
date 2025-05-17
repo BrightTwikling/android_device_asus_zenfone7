0ba3b09e0f536024f66d6d7a0db52bf85f5c8cab
BrightTwikling <DFSJbbba5@gmail.com>
2025-05-17 21:05:35 +0900

    Bluetooth: Add PRODUCT_PACKAGES
    Signed-off-by: BrightTwikling <DFSJbbba5@gmail.com>

--- device.mk
+++ device.mk
@@ -154,7 +154,11 @@ PRODUCT_COPY_FILES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.1-impl \
-    audio.bluetooth.default
+    audio.bluetooth.default \
+    android.hardware.bluetooth@1.0 \
+    android.hardware.bluetooth.audio@2.0 \
+    android.hardware.bluetooth.audio@2.1 \
+    vendor.qti.hardware.bluetooth_sar@1.0
 
 PRODUCT_COPY_FILES +=\
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
