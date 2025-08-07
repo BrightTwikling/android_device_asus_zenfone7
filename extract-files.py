2be58443196226d39a38f4e0a251af3909df79ca
BrightTwikling <DF.SJbbba5@gmail.com>
2025-08-07 17:46:26 +0900

    Fix typo at extract-files.py
    Signed-off-by: BrightTwikling <DF.SJbbba5@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -35,7 +35,7 @@ lib_fixups: lib_fixups_user_type = {
     (
         'com.qualcomm.qti.dpm.api@1.0',
         'vendor.qti.imsrtpservice@3.0',
-        'ibAsusMotorDriverHAL'
+        'libAsusMotorDriverHAL'
         'libxditk_DIT_Manager',
         'libxditk_ISP',
         'libxditk_arch',
