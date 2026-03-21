8919ec62576a240ee7893bb21032838d6d5cfcce
BrightTwikling <DF.SJbbba5@gmail.com>
2026-03-21 22:04:22 +0900

    zenfone7: Update Lineage Health to soong_config_set
    Inspired by following commit
    https://github.com/LineageOS/android_device_samsung_universal9830-common/commit/dd5a4dad
    
    Signed-off-by: BrightTwikling <DF.SJbbba5@gmail.com>

--- device.mk
+++ device.mk
@@ -346,10 +346,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
-$(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
-$(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
-$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/input_suspend)
-$(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
+$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/charging_enabled)
+$(call soong_config_set,lineage_health,charging_control_charging_enabled,1)
+$(call soong_config_set,lineage_health,charging_control_charging_disabled,0)
+$(call soong_config_set,lineage_health,charging_control_charging_bypass,true)
+$(call soong_config_set,lineage_health,charging_control_charging_toggle,true)
+$(call soong_config_set,lineage_health,charging_control_charging_deadline,false)
 
 # HIDL
 PRODUCT_PACKAGES += \
